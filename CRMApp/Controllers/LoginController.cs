63a6293f2ad616d045b26654a2ea36b7e66ae0aa
KFT_guochengfu <kft_guochengfu@bingosoft.net>
2022-10-16 12:18:11 -0500

    添加项目文件。

---
+++ CRMApp/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRMApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CRMApp/DBContext.cs
@@ -0,0 +1,20 @@
+﻿using CRMAppEntity;
+using Microsoft.EntityFrameworkCore;
+using System;
+namespace CRMApp
+{
+    public class DBContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=LAPTOP-R0ORLLEH;Initial Catalog=Order;User ID=sa;Password=111111");
+            optionsBuilder.LogTo(Console.WriteLine);
+        }
+
+        public virtual DbSet<Customer> CustomerSet { get; set; }
+        public virtual DbSet<CreditCard> CreditCardSet { get; set; }
+        public virtual DbSet<User> UserSet { get; set; }
+    }
+   
+}
